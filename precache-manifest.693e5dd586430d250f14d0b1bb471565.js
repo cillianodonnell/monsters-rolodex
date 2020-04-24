525f73b89698c66fa5e9dd9e480836493ce30fea
Cillian O'Donnell <cpodonnell8@gmail.com>
2020-04-24 08:30:57 +0100

    Updates

---
+++ precache-manifest.693e5dd586430d250f14d0b1bb471565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1606961ce7fcdda59bb8f46545a0b8eb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e08b0ab94511ee43756e",
+    "url": "/monsters-rolodex/static/css/main.deaa7c8b.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08b0ab94511ee43756e",
+    "url": "/monsters-rolodex/static/js/main.47b69662.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
